09c875cf3f1a8d3254d4f5ed93c69ca1096994b4
kampamba chanda <kampambachanda@gmail.com>
2016-03-09 00:45:50 +0200

    alpha. ready for testing

--- filestem.py
+++ filestem.py
@@ -2,12 +2,19 @@
 # -*- coding:utf-8 -*-
 
 import pygwizo
+import time
 
-array = open("input.txt").read().splitlines()
-for word in array:
-    val = pygwizo.Ingest(word)
-    deep = val.deep_stem()
-    with open("stem.txt", "a") as file:
-        stem = deep + "\n"
-        file.writelines(stem)
-        print(word, " ---> ", deep)
+if __name__ == "__main__":
+    array = open("input.txt").read().splitlines()
+    start_time = time.time()
+    for word in array:
+        val = pygwizo.Ingest(word)
+        deep = val.deep_stem()
+        with open("stem.txt", "a") as file:
+            stem = deep + "\n"
+            file.writelines(stem)
+            print(word, " ---> ", deep)
+
+    print("============================")
+    print("Done After: %ss" % (time.time() - start_time))
+    print("============================")
